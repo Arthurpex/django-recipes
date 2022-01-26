2435b5c375c5d43f7043eb1e4dfcd9b215776e0c
Arthurpex <arthurpex@gmail.com>
2022-01-26 14:33:10 -0300

    feature: adds superuser

--- app/core/models.py
+++ app/core/models.py
@@ -1,14 +1,29 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
+    PermissionsMixin
+
 
 class UserManager(BaseUserManager):
     """Custom UserManager Class"""
+
     def create_user(self, email, password=None, **extra_fields):
         """Creates and saves a new user"""
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError('Users must have a email address')
+
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
+
+    def create_superuser(self, email, password):
+        """Creates and saves a new superuser"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()
+        return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """Custom User model"""
