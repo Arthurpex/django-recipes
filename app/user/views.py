741c407e025050fb9a741c327b088e4a80da4cc0
Arthurpex <arthurpex@gmail.com>
2022-01-27 10:30:06 -0300

    fix typo in serializers

--- app/user/views.py
+++ app/user/views.py
@@ -15,7 +15,7 @@ class CreateTokenView(ObtainAuthToken):
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
 
-class ManageUserView(generics.RetrieveAPIView):
+class ManageUserView(generics.UpdateAPIView):
     """Manage the authenticated user"""
     serializer_class = UserSerializer
     authentication_classes = (authentication.TokenAuthentication,)
